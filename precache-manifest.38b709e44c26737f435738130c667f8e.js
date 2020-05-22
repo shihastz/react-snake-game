bb3bf8593d0a9209cdcd15afc4a2137d55859fae
shihastz <shihast0503@gmail.com>
2020-05-22 21:55:11 +0530

    Updates

---
+++ precache-manifest.38b709e44c26737f435738130c667f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133b70cc29a976a83810883507f145d4",
+    "url": "/react-snake-game/index.html"
+  },
+  {
+    "revision": "c89fc7d87bebf67cc378",
+    "url": "/react-snake-game/static/css/main.8563c2c8.chunk.css"
+  },
+  {
+    "revision": "67ab39851778d6050efd",
+    "url": "/react-snake-game/static/js/2.6e0b983d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-snake-game/static/js/2.6e0b983d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89fc7d87bebf67cc378",
+    "url": "/react-snake-game/static/js/main.f886218c.chunk.js"
+  },
+  {
+    "revision": "3aedf9438c9c8d6fcd48",
+    "url": "/react-snake-game/static/js/runtime-main.6848036d.js"
+  }
+]);
